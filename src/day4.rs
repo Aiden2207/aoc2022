96f5ec0d85733d757511853adb70ad50fdb7e46d
Aiden2207 <ajsandbakken@comcast.net>
2022-12-03 23:17:33 -0600

    did day 4

--- src/day4.rs
+++ src/day4.rs
@@ -1,1 +1,48 @@
+use itertools::Itertools;
 
+pub fn part1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|s| {
+            let (l, r) = s
+                .split(',')
+                .map(|r| {
+                    let (start, end) = r
+                        .split('-')
+                        .map(|n| n.parse::<u32>().unwrap())
+                        .next_tuple()
+                        .unwrap();
+                    start..=end
+                })
+                .next_tuple()
+                .unwrap();
+            (l.contains(r.start()) && l.contains(r.end()))
+                || (r.contains(l.end()) && r.contains(l.start()))
+        })
+        .filter(|b| *b)
+        .count() as _
+}
+pub fn part2(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|s| {
+            let (l, r) = s
+                .split(',')
+                .map(|r| {
+                    let (start, end) = r
+                        .split('-')
+                        .map(|n| n.parse::<u32>().unwrap())
+                        .next_tuple()
+                        .unwrap();
+                    start..=end
+                })
+                .next_tuple()
+                .unwrap();
+            l.contains(r.start())
+                || l.contains(r.end())
+                || r.contains(l.end())
+                || r.contains(l.start())
+        })
+        .filter(|b| *b)
+        .count() as _
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use aoc2022::day3::part2;
+use aoc2022::day4::part2;
 use aoc2022::read_data;
 fn main() {
-    let s = read_data("day3.txt");
+    let s = read_data("day4.txt");
     println!("day2: {:?}", part2(&s));
 }
