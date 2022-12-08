3d8f3be05fe6fc9943f21cd1dc69e82b8366fa03
Aiden2207 <ajsandbakken@comcast.net>
2022-12-07 21:50:54 -0600

    added basic framework

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub mod day4;
 pub mod day5;
 pub mod day6;
 pub mod day7;
+pub mod day8;
 pub fn read_data(p: impl AsRef<Path>) -> String {
     let path = PathBuf::from("data").join(p);
     std::fs::read_to_string(path).unwrap()
